07c1f08d82e583ba37adfcb9b5efcb9fce2f5f98
Grzegorz Pach <grzegorz.andrzej.pach@gmail.com>
2024-06-18 13:46:45 +0000

    Fix for codesandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
-import { Page1 } from './pages/Page1';
-import { Page2 } from './pages/Page2';
-import { Page3 } from './pages/Page3';
-import { AnimatedRoutes } from './components/AnimatedRoutes';
+import React from "react";
+import { MemoryRouter, Route } from "react-router-dom";
+import { Page1 } from "./pages/Page1";
+import { Page2 } from "./pages/Page2";
+import { Page3 } from "./pages/Page3";
+import { AnimatedRoutes } from "./components/AnimatedRoutes";
 
 function App() {
   return (
     <MemoryRouter>
-    <AnimatedRoutes>
-      <Route element={<Page1 />} path='/' />
-      <Route element={<Page2 />} path='/page2' />
-      <Route element={<Page3 />} path='/page3' />
-    </AnimatedRoutes>
+      <AnimatedRoutes>
+        <Route element={<Page1 />} path="/" />
+        <Route element={<Page2 />} path="/page2" />
+        <Route element={<Page3 />} path="/page3" />
+      </AnimatedRoutes>
     </MemoryRouter>
   );
 }
